9f092f64d03ea2f94fdf769f9f157938186c11db
teotimepacreau <95431443+teotimepacreau@users.noreply.github.com>
2024-02-07 16:05:06 +0100

    trying to Tina Cloud

--- tina/config.js
+++ tina/config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from "tinacms";
 
-// Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "tina";
 
 export default defineConfig({
-  branch,
+  branch: "tina",
 
   // Get this from tina.io
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,

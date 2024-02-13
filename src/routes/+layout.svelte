122b05192798d428c485f9c8ddb45eb1ce198c42
teotimepacreau <95431443+teotimepacreau@users.noreply.github.com>
2024-02-13 08:41:40 +0100

    build(search pagefind)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,23 +21,27 @@
   import { onMount } from "svelte";
 
   // SEARCH START
-  let dialog;
+  let closeDialogWhenClickOutsideTheBox;
+  let openDialog
+  let dialog
 
   onMount(() => {
-    //onMount car sinon la sélection run avant que le composant soit rendu
     dialog = document.querySelector("#search-dialog");
-  });
 
-  const openDialog = () => {
+    //onMount car sinon la sélection run avant que le composant soit rendu
+    closeDialogWhenClickOutsideTheBox = (event) => {
+
+      if (event.target == dialog) {
+        //car le dialog prend tout le reste de la page, l'intérieur de la boite c'est le <search>
+        dialog.close();
+      }
+    };
+    openDialog = () => {
     dialog.showModal();
   };
+  });
 
-  const closeDialogWhenClickOutsideTheBox = (event) => {
-    if (event.target == dialog) {
-      //car le dialog prend tout le reste de la page, l'intérieur de la boite c'est le <search>
-      dialog.close();
-    }
-  };
+  
 
   // PAGEFIND
   let query = "";
@@ -61,36 +65,38 @@
     if (pagefindMessage) {
       pagefindMessage.textContent = `${numberOfResults} résultats pour "${query}"`;
     } else {
-      
       pagefindMessage = document.createElement("p");
       pagefindMessage.classList.add("pagefind-message");
       pagefindMessage.textContent = `${numberOfResults} résultats pour "${query}"`;
     }
 
     // LISTE DE RESULTS
-    let pagefindQueryResults = document.createElement('ol')
+    let pagefindQueryResults = document.createElement("ol");
 
     for (const result of r.results) {
       let data = await result.data(); //obligé d'await result.data pour bien avoir les résultats de la query
-      console.log('data', data)
-      pagefindQueryResults.innerHTML += 
-        `
-          <li>
-            <a href="${data.url.replace('.html', '')}">${data.meta.title}<a/>
-          </li>
-        `
+      console.log("data", data);
+      pagefindQueryResults.innerHTML += `
+        <li><a class="search-result" href="${data.url.replace(".html", "")}">${
+          data.meta.title
+        }</a></li>
+        `;
     }
-
-
-
+   
     docFrag.appendChild(pagefindMessage);
-
-    docFrag.appendChild(pagefindQueryResults)
-
+    docFrag.appendChild(pagefindQueryResults);
     resultsContainer.appendChild(docFrag);
 
-    
+    // FERMER LE DIALOG SI CLIC SUR UN RESULTAT DE RECHERCHE
+    let allSearchResults = document.querySelectorAll('.search-result')
+      console.log('allSearchResults', allSearchResults)
+      allSearchResults.forEach((result)=>{
+        result.addEventListener('click',() => {
+          result.closest('#search-dialog').close()
+        })
+      })
   };
+
 </script>
 
 <svelte:head>
@@ -128,11 +134,7 @@
   </button>
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <!-- svelte-ignore a11y-no-static-element-interactions -->
-  <dialog
-    on:click|preventDefault={closeDialogWhenClickOutsideTheBox}
-    on:keyup|preventDefault={closeDialogWhenClickOutsideTheBox}
-    id="search-dialog"
-  >
+  <dialog on:click={closeDialogWhenClickOutsideTheBox} id="search-dialog">
     <div id="inner-dialog" class="relative">
       <search
         id="search-box-open"
@@ -197,5 +199,4 @@
   :global(#results ol) {
     @apply py-4;
   }
-
 </style>
